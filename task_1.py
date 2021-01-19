06a52ef2e3e30a794e2786f04b9b81cd378b55af
Geek101-prog <notforfun23@gmail.com>
2021-01-19 15:34:44 +0300

    task's for homework number № 1

---
+++ task_1.py
@@ -0,0 +1,6 @@
+first_name = input('First name')
+age = int(input('Age'))
+weight = int(input('Weight'))
+print('Your first name %s, you age %d and your weight %d' % (first_name, age, weight))
+print('Your first name {}, you age {} and your weight {}'.format(first_name, age, weight))
+print(f'Your first name {first_name}, you age {age} and your weight {weight}')
---
+++ task_3.py
@@ -0,0 +1,5 @@
+number = int(input('Enter the number'))
+number_sum1 = str(number) + str(number)
+number_sum2 = str(number) + str(number) + str(number)
+number_sum3 = number + int(number_sum1) + int(number_sum2)
+print(number_sum3)
