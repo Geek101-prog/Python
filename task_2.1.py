4adc17cdfa79e12a8c2527a0d421e191849b7583
Geek101-prog <notforfun23@gmail.com>
2021-01-25 22:42:25 +0300

    1/1

--- task_2.1.py
+++ task_2.1.py
@@ -1,3 +1,6 @@
 my_set = [1, 1.2, 'How i met your mother', (5 + 6j), True, None]
 for x, i in enumerate(my_set, 1):
     print(x, type(i))
+print('*' * 33)
+for i, item in enumerate(my_set, 1):
+    print(f'{i} {item} - {type(item)}')
--- task_2.2.py
+++ task_2.2.py
@@ -11,3 +11,10 @@ for i in range(int(len(my_list) // 2)):
     my_list[y], my_list[y + 1] = my_list[y + 1], my_list[y]
     y += 2
 print(my_list)
+
+print('*' * 33)
+
+my_list_1 = list(input('Enter your numbers without space -'))
+for i in range(1, len(my_list_1), 2):
+    my_list[i - 1], my_list_1[i] = my_list_1[i], my_list_1[i - 1]
+print(my_list_1)
